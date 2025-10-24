b5e4009ec53615799ee48b9a130df539cb4e02c7
pranshu-tomer <tomerpranshu11@gmail.com>
2025-10-24 16:04:42 +0530

    Updating Data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Pranshu Tomer',
     title: "Full Stack Developer | MERN & Java Backend",
     desciption:"I’m a passionate Full Stack Developer skilled in building scalable web applications using React.js, Node.js, Express, and MongoDB. I enjoy solving complex problems through clean code, smart design, and modern technologies. With a solid foundation in Java, DSA, and Machine Learning, I’m always eager to explore new tools, learn fast, and craft impactful digital solutions.",
-    image: '',
+    image: './profile_photo.jpg',
     resumePdf: resume
 }
