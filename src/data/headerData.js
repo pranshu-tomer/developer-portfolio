576b6c1b47e8e0f0ef561a57a35c3c3a1e9cd43b
pranshu-tomer <tomerpranshu11@gmail.com>
2025-10-24 15:47:29 +0530

    Updating Data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    name: 'Pranshu Tomer',
+    title: "Full Stack Developer | MERN & Java Backend",
+    desciption:"I’m a passionate Full Stack Developer skilled in building scalable web applications using React.js, Node.js, Express, and MongoDB. I enjoy solving complex problems through clean code, smart design, and modern technologies. With a solid foundation in Java, DSA, and Machine Learning, I’m always eager to explore new tools, learn fast, and craft impactful digital solutions.",
+    image: './profile_photo.jpg',
     resumePdf: resume
 }
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -8,78 +8,98 @@ import seven from '../assets/svg/projects/seven.svg'
 import eight from '../assets/svg/projects/eight.svg'
 
 
+
+
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Employee Management',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Number Guess Game',
+        projectDesc: 'This project is purely based on JavaScript.Game is about guessing the random number.',
+        tags: ['JavaScript', 'HTML', 'CSS'],
+        code: 'https://github.com/pranshu-tomer/guessNumber-game',
+        demo: 'https://shadow-voltrex-guess-number.netlify.app/',
         image: one
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Password Generator',
+        projectDesc: 'This project is based on ReactJs.  You can create password of diffrent length.',
+        tags: ['React', 'CSS'],
+        code: 'https://github.com/pranshu-tomer/passwordGenerator-react',
+        demo: 'https://shadow-voltrex-password-generator.netlify.app/',
         image: two
     },
     {
         id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'TodoList-React',
+        projectDesc: "It might sound basic, but it's been quite fun and educational. For state management , I used contextAPI. I used a hook called useEffect to set and get the items in localStorage.",
+        tags: ['React', 'CSS'],
+        code: 'https://github.com/pranshu-tomer/todo-list-react',
+        demo: 'https://shadow-voltrex-todo-list.netlify.app/',
         image: three
     },
     {
         id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'FrontEnd',
+        projectDesc: 'A sleek side navigation bar implementation. Mini frontend project , just for Fun',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        code: 'https://github.com/pranshu-tomer/Mini-Project-html-css',
+        demo: 'https://phenomenal-nougat-2c14ff.netlify.app/',
         image: four
     },
     {
         id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'UsabilityHub Clone',
+        projectDesc: 'A frontend replica of the UsabilityHub site! I put it together while mastering HTML and CSS on my journey to becoming a full-stack developer. But keep in mind, my version isn\'t responsive yet.',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        code: 'https://github.com/pranshu-tomer/UsabilityHub---Clone',
+        demo: 'https://mellow-bombolone-6a0655.netlify.app/',
         image: five
     },
     {
         id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Simon Game',
+        projectDesc: 'JavaScript-based project: the Simon Says game. With four color boxes, players must remember and repeat the sequence of blinking colors. Each level adds a new blinking color, challenging players to recall the sequence accurately. Test your memory and reflexes with Simon Says!',
+        tags: ['JavaScript', 'HTML', 'CSS'],
+        code: 'https://github.com/pranshu-tomer/javaScript-project',
+        demo: 'https://guileless-sprite-6827eb.netlify.app/',
         image: six
     },
     {
         id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'NIT Students Portal',
+        projectDesc: 'A portal for NIT students to access resources, connect with peers, and stay updated on campus events.',
+        tags: ['Node.js', 'Express', 'EJS', 'MongoDB'],
+        code: 'https://github.com/pranshu-tomer/nit-students',
+        demo: 'https://nit-students-sv.onrender.com',
         image: seven
     },
     {
         id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'WandarLust - Hotel Booking',
+        projectDesc: 'A platform for hotel owners to list their properties and for travelers to find and review hotels.',
+        tags: ['MERN', 'React', 'Node.js', 'Express', 'MongoDB'],
+        code: 'https://github.com/pranshu-tomer/WanderLust',
+        demo: 'https://wanderlust-ll2g.onrender.com',
         image: eight
+    },
+    {
+        id: 9,
+        projectName: 'Fruit Predictor',
+        projectDesc: 'Based on machine learning.It predict the fruit name according to its physical appearance like color,weight and size etc.',
+        tags: ['Python', 'Machine Learning'],
+        code: 'https://github.com/pranshu-tomer/fruit-predictor',
+        demo: '',
+        image: one
+    },
+    {
+        id: 10,
+        projectName: 'Laptop Price Predictor',
+        projectDesc: 'Based on machine learning.It predict the Laptop price according to its specifications like RAM,size and storage etc.',
+        tags: ['Python', 'Machine Learning','matplotlib'],
+        code: 'https://github.com/pranshu-tomer/laptop-price-predictor',
+        demo: '',
+        image: one
     },
 ]
 
